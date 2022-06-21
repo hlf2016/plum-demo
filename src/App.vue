2f658e8e66d64226a86831d8ed14a523d6ca3026
Pannio <1821299140@qq.com>
2022-06-21 23:11:16 +0800

    chore:clean up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
-    <Footer />
-  </main>
+  <div>
+    Hi
+  </div>
 </template>
+
+<style scoped>
+</style>
